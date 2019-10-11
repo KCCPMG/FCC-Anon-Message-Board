fcc907fbcf87755837c9b8c9f83814d8152f79b3
Glitch (faceted-arrow) <none>
2019-10-11 23:03:55 +0000

    🦁🍃 Checkpoint ./server.js:6206543/111

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(helmet.frameguard({action: 'sameorigin'}));
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.referrerPolicy({policy: 'same-origin'}));
 
 //Sample front-end
 app.route('/b/:board/')
