e6a642e06a2369b1cdd8b3d0a5ac9e1fcb55fead
Glitch (faceted-arrow) <none>
2019-10-15 01:28:06 +0000

    🛬🏄 Checkpoint ./routes/api.js:6206543/185

--- routes/api.js
+++ routes/api.js
@@ -83,7 +83,7 @@ var Thread = mongoose.model('thread', threadSchema);
 
 
 /*
-I can POST a thread to a specific message board by passing form data text and delete_password to /api/threads/{board}.(Recomend res.redirect to board page /b/{board}) Saved will be _id, text, created_on(date&time), bumped_on(date&time, starts same as created_on), reported(boolean), delete_password, & replies(array).
+x - I can POST a thread to a specific message board by passing form data text and delete_password to /api/threads/{board}.(Recomend res.redirect to board page /b/{board}) Saved will be _id, text, created_on(date&time), bumped_on(date&time, starts same as created_on), reported(boolean), delete_password, & replies(array).
 I can POST a reply to a thead on a specific board by passing form data text, delete_password, & thread_id to /api/replies/{board} and it will also update the bumped_on date to the comments date.(Recomend res.redirect to thread page /b/{board}/{thread_id}) In the thread's 'replies' array will be saved _id, text, created_on, delete_password, & reported.
 I can GET an array of the most recent 10 bumped threads on the board with only the most recent 3 replies from /api/threads/{board}. The reported and delete_passwords fields will not be sent.
 I can GET an entire thread with all it's replies from /api/replies/{board}?thread_id={thread_id}. Also hiding the same fields.
@@ -124,5 +124,9 @@ module.exports = function (app) {
   app.route('/api/replies/:board')
   .post(function(req, res){
     var board = req.params.board;
+    var id = req.body.thread_id;
+    var text = req.body.text;
+    var deletePassword = req.body.delete_password;
+    
   })
 };
